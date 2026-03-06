29c1a81e09a8c3ca83be9341d10c2671cd06cca3
ibrahimosman786 <ibrahimosman1040@gmail.com>
2026-03-06 16:09:23 -0700

    RED: Add hasCity test (should fail)

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -1,7 +1,36 @@
 package com.example.lab08;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
 public class CustomListTest {
 
+    private CustomList list;
+
+    @BeforeEach
+    public void setUp() {
+        list = new CustomList();
+    }
+
+    @Test
+    public void testHasCityTrue() {
+        City calgary = new City("Calgary", "AB");
+        list.addCity(calgary);
+        assertTrue(list.hasCity(calgary));
+    }
+
+    @Test
+    public void testHasCityFalse() {
+        City calgary = new City("Calgary", "AB");
+        City edmonton = new City("Edmonton", "AB");
+        list.addCity(calgary);
+        assertFalse(list.hasCity(edmonton));
+    }
+
+    @Test
+    public void testHasCityEmptyList() {
+        City city = new City("Vancouver", "BC");
+        assertFalse(list.hasCity(city));
+    }
 }
