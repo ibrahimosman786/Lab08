b301451a98f5d2e3695b6bf510da4f97d53eb0e6
ibrahimosman786 <ibrahimosman1040@gmail.com>
2026-03-06 16:14:23 -0700

    RED: Add deleteCity test (should fail)

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -13,6 +13,7 @@ public class CustomListTest {
         list = new CustomList();
     }
 
+    // ===== hasCity tests =====
     @Test
     public void testHasCityTrue() {
         City calgary = new City("Calgary", "AB");
@@ -32,5 +33,24 @@ public class CustomListTest {
     public void testHasCityEmptyList() {
         City city = new City("Vancouver", "BC");
         assertFalse(list.hasCity(city));
+    }
+
+    // ===== deleteCity tests =====
+    @Test
+    public void testDeleteCity() {
+        City calgary = new City("Calgary", "AB");
+        list.addCity(calgary);
+        list.deleteCity(calgary);
+        assertEquals(0, list.getCount());
+        assertFalse(list.hasCity(calgary));
+    }
+
+    @Test
+    public void testDeleteCityNotInList() {
+        City calgary = new City("Calgary", "AB");
+        City edmonton = new City("Edmonton", "AB");
+        list.addCity(calgary);
+        list.deleteCity(edmonton);
+        assertTrue(list.hasCity(calgary));
     }
 }
